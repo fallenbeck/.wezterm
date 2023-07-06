15a6c9de3a23e7d3f9e3c7a17a9d00df27290168
Niels Fallenbeck <niels@fallenbeck.com>
2023-07-06 19:59:58 +0200

    Set startup dimensions and smaller fixes

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,5 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
-local gpus = wezterm.gui.enumerate_gpus()
 
 -- This table will hold the configuration.
 local config = {}
@@ -15,7 +14,11 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
---
+
+-- Initial terminal size
+config.initial_cols = 132
+config.initial_rows = 42
+
 -- Font
 config.font = wezterm.font('Hack Nerd Font')
 
@@ -31,9 +34,9 @@ config.color_scheme = 'GruvboxDark'
 config.enable_tab_bar = false
 
 -- Set the opacity of the terminal window
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.85
 -- config.text_background_opacity = 0.5
-config.macos_window_background_blur = 40
+config.macos_window_background_blur = 30
 
 -- Fixes to get [] and tilde ~ on german keyboards
 config.use_ime = true
@@ -43,51 +46,50 @@ config.use_dead_keys = true
 
 -- Clickable hyperlinks
 -- make clickable hyperlinks work
-hyperlink_rules = {
+config.hyperlink_rules = {
     -- Linkify things that look like URLs and the host has a TLD name.
     -- Compiled-in default. Used if you don't specify any hyperlink_rules.
     {
-      regex = '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b',
-      format = '$0',
+        regex = '\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b',
+        format = '$0',
     },
     -- linkify email addresses
     -- Compiled-in default. Used if you don't specify any hyperlink_rules.
     {
-      regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
-      format = 'mailto:$0',
+        regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
+        format = 'mailto:$0',
     },
     -- file:// URI
     -- Compiled-in default. Used if you don't specify any hyperlink_rules.
     {
-      regex = [[\bfile://\S*\b]],
-      format = '$0',
+        regex = [[\bfile://\S*\b]],
+        format = '$0',
     },
     -- Linkify things that look like URLs with numeric addresses as hosts.
     -- E.g. http://127.0.0.1:8000 for a local development server,
     -- or http://192.168.1.1 for the web interface of many routers.
     {
-      regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
-      format = '$0',
+        regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
+        format = '$0',
     },
     -- Make task numbers clickable
     -- The first matched regex group is captured in $1.
     {
-      regex = [[\b[tT](\d+)\b]],
-      format = 'https://example.com/tasks/?t=$1',
+        regex = [[\b[tT](\d+)\b]],
+        format = 'https://example.com/tasks/?t=$1',
     },
     -- Make username/project paths clickable. This implies paths like the following are for GitHub.
     -- ( "nvim-treesitter/nvim-treesitter" | wbthomason/packer.nvim | wez/wezterm | "wez/wezterm.git" )
     -- As long as a full URL hyperlink regex exists above this it should not match a full URL to
     -- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
     {
-      regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-      format = 'https://www.github.com/$1/$3',
+        regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+        format = 'https://www.github.com/$1/$3',
     },
- }
+}
 
-
-
--- WezTerm does not start in my Proxmox VM
+-- WezTerm does not start in a Proxmox VM - debug output
+-- local gpus = wezterm.gui.enumerate_gpus()
 -- print(gpus)
 -- config.front_end = "WebGpu"
 -- config.webgpu_force_fallback_adapter = true
